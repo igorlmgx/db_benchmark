1f027f77b28f0efff870cfa373ed67056242621e
igorlmgx <igor.guedes@quintoandar.com.br>
2023-05-30 17:18:21 -0300

    fix sqflite database creation

--- lib/databases/sqflite.dart
+++ lib/databases/sqflite.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
 import 'package:sqflite/sqflite.dart';
 import 'dart:async';
 
@@ -8,8 +12,15 @@ class SqfliteDB {
   static const visualizedPinsTableIdxBoth = 'VisualizedPinsIdxBoth';
 
   static Future<Database> open() async {
+    final dir = await getApplicationDocumentsDirectory();
+    final dbPath = p.join(dir.path, 'sqlite.db');
+
+    if (await File(dbPath).exists()) {
+      await File(dbPath).delete();
+    }
+
     return await openDatabase(
-      'sqflite_benchmark.db',
+      dbPath,
       version: 1,
       onCreate: (db, version) async {
         await db.execute('''
@@ -18,32 +29,44 @@ class SqfliteDB {
             houseId TEXT UNIQUE NOT NULL,
             visualizedAt INTEGER NOT NULL
           );
+          ''');
 
+        await db.execute('''
           CREATE TABLE $visualizedPinsTableIdxHouseId (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             houseId TEXT UNIQUE NOT NULL,
             visualizedAt INTEGER NOT NULL
           );
+          ''');
 
+        await db.execute('''
           CREATE UNIQUE INDEX idx_houseId
           ON $visualizedPinsTableIdxHouseId (houseId);
+          ''');
 
+        await db.execute('''
           CREATE TABLE $visualizedPinsTableIdxDate (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             houseId TEXT UNIQUE NOT NULL,
             visualizedAt INTEGER NOT NULL
           );
+          ''');
 
-          CREATE UNIQUE INDEX idx_date
+        await db.execute('''
+          CREATE INDEX idx_date
           ON $visualizedPinsTableIdxDate (visualizedAt);
+          ''');
 
+        await db.execute('''
           CREATE TABLE $visualizedPinsTableIdxBoth (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             houseId TEXT UNIQUE NOT NULL,
             visualizedAt INTEGER NOT NULL
           );
+          ''');
 
-          CREATE UNIQUE INDEX idx_id_date
+        await db.execute('''
+          CREATE INDEX idx_id_date
           ON $visualizedPinsTableIdxBoth (houseId, visualizedAt);
         ''');
       },
